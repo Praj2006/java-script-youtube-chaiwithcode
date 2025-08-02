61303314f1a43e3a2453c40aee1809fde3674cda
Praj2006 <prashnt2006@gmail.com>
2025-08-02 13:14:13 +0000

    containetr config

--- .devcontainer/01-Basics/02-Datatypes.js
+++ .devcontainer/01-Basics/02-Datatypes.js
@@ -3,3 +3,21 @@
 /* We are Using NodeJs, Not Browser*/
 console.log(3+3) // Code Readability should be High
 console.log("Prashant Raj")
+let name="Prashant"
+let age=19
+let isLoggedIn=false
+let state; // undefined
+let prof=null // Stand Alone Function
+// number=2 to the power 53
+// BigInt 
+//String=>""
+//Boolean=> "True"or"False"
+//Null => Stand alone value
+//UnDefined=> "Abhi Value Define Nhi Hui Hai"
+//Symbol=> Unique (Used manily in React)
+//Object==> 
+console.log(typeof"Prashant");
+console.log(typeof null);
+console.log(typeof undefined);
+    
+    
