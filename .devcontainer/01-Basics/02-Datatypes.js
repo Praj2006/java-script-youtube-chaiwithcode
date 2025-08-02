1ad13131be0fd5dcfb446ebfa1feed3646fe26f2
Praj2006 <prashnt2006@gmail.com>
2025-08-02 12:54:14 +0000

    container config

---
+++ .devcontainer/01-Basics/02-Datatypes.js
@@ -0,0 +1,5 @@
+"Use Strict";//Treat all JS code as newer Version
+// alert(3+3)
+/* We are Using NodeJs, Not Browser*/
+console.log(3+3) // Code Readability should be High
+console.log("Prashant Raj")
