847e7eddd857c0703e35ea3d436faa50bfd89926
Praj2006 <prashnt2006@gmail.com>
2025-08-02 12:21:17 +0000

    container config

---
+++ .devcontainer/01-Basics/01-Variables.js
@@ -0,0 +1,16 @@
+const accountId=144553
+let accountEmail="prashnt2006@gmail.com"
+var accountPassword="12345"
+accountCity="Jaipur"
+//accountId=2// not allowed
+//let is used inplace of var
+accountEmail="HC@hc.com"
+accountPassword="21212121212"
+accountCity="Bengaluru"
+console.log(accountCity);
+console.table([accountId,accountEmail,accountPassword,accountCity])
+/*
+Prefer Not to use var
+Because of issue in block Scope and Functional Scope
+*/
+
