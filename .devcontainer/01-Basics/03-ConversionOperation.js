0de2caae266487bb6364b083869de7e890fec0c2
Praj2006 <prashnt2006@gmail.com>
2025-08-04 03:12:31 +0000

    .container config

---
+++ .devcontainer/01-Basics/03-ConversionOperation.js
@@ -0,0 +1,65 @@
+let score ="33"
+
+console.log(typeof score)
+console.log(typeof(score))
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+let score1 ="HITESH"
+
+console.log(typeof score1)
+console.log(typeof(score1))
+let valueInNumber1=Number(score1)
+console.log(typeof valueInNumber1)
+console.log(valueInNumber1)
+
+let score2 =null
+
+console.log(typeof score2)
+console.log(typeof(score2))
+let valueInNumber2=Number(score2)
+console.log(typeof valueInNumber2)
+console.log(valueInNumber2)
+
+let score3 =undefined
+
+console.log(typeof score3)
+console.log(typeof(score3))
+let valueInNumber3=Number(score3)
+console.log(typeof valueInNumber3)
+console.log(valueInNumber3)
+
+let score4 =true
+
+console.log(typeof score4)
+console.log(typeof(score4))
+let valueInNumber4=Number(score3)
+console.log(typeof valueInNumber4)
+console.log(valueInNumber4)
+//"33"=> 33
+//"33abc"=> NaN
+// true=> 1; False=> 0
+
+let isLoggedIn=1
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+let isLoggedIn1="PRASHANT RAJ"
+let booleanIsLoggedIn1=Boolean(isLoggedIn1)
+console.log(booleanIsLoggedIn1)
+
+let isLoggedIn2=""
+let booleanIsLoggedIn2=Boolean(isLoggedIn2)
+console.log(booleanIsLoggedIn2)
+
+// 1=> TRUE ; 0=> FALSE
+// ""=> FALSE
+//"PRASHANT RAJ"=>TRUE
+
+let somenumber=33
+let stringNumber= String(somenumber)
+console.log(typeof stringNumber);
+
+
+
